91d0b8e4ec3647c068bb8ef7ba51126d1ca67acc
Valeriy <grishko28@gmail.com>
2019-07-01 14:28:46 +0300

    add Dialogs

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navigation from './components/Navigation/Navigation';
 import Profile from './components/profile/profile';
+import Dialogs from "./components/Dialogs/Dialogs";
 
 const App = () => {
   return (
     <div className="app-wrapper">
       <Header/>
       <Navigation/>
-      <Profile/>
+
+      <div className="app-wrapper-content">
+          <Dialogs/>
+          {/*<Profile/>*/}
+      </div>
+
     </div>
   );
 }
