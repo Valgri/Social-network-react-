d718046f4d037e43406c4c9f3a79eaf907e8f09d
Valeriy <grishko28@gmail.com>
2019-07-02 13:34:55 +0300

    dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import style from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={style.dialogs}>
+            <div className={style.dialogsItems}>
+                <div className={style.dialog + ' ' + style.active}>
+                    <NavLink to='/dialogs/1'> Grisha</NavLink>
+                </div>
+                <div className={style.dialog}>
+                    <NavLink to='/dialogs/2'>  Danil </NavLink>
+                </div>
+                <div className={style.dialog}>
+                    <NavLink to='/dialogs/3'> Vitaliy </NavLink>
+                </div>
+                <div className={style.dialog}>
+                    <NavLink to='/dialogs/4'> Yana </NavLink>
+                </div>
+            </div>
+            <div className={style.messages}>
+                <div className={style.dialog}>
+                    Hello!
+                </div>
+                <div className={style.dialog}>
+                    Hi!
+                </div>
+            </div>
         </div>
+
     )
 }
 
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -5,8 +5,8 @@ import {NavLink} from "react-router-dom";
 const Navigation = () => {
     return  (
         <nav className={classes.nav}>
-            <div className={classes.item}><NavLink to='/profile'>Profile</NavLink></div>
-            <div className={classes.item}><NavLink to='/dialogs'>Messages</NavLink></div>
+            <div className={classes.item}><NavLink to='/profile' activeClassName={classes.activeLink}>Profile</NavLink></div>
+            <div className={classes.item}><NavLink to='/dialogs' activeClassName={classes.activeLink}>Messages</NavLink></div>
             <div className={classes.item}><a>News</a></div>
             <div className={classes.item}><a>Music</a></div>
             <div className={classes.item}><a>Settings</a></div>
