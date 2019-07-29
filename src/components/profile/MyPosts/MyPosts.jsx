94f2c6e37b438724055719029b54029572b8e32f
Valeriy <grishko28@gmail.com>
2019-07-29 11:38:49 +0300

    addPost onClick

--- src/components/profile/MyPosts/MyPosts.jsx
+++ src/components/profile/MyPosts/MyPosts.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import style from './MyPosts.module.css';
 import Post from './Post/Post';
 
-
+// =)
 const MyPosts = (props) => {
 
     let newPostElement = React.createRef();
